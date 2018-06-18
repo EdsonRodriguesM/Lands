a1399fefe332154bcd150e6513536d2b0b5f9e5b
Edson Rodrigues Macedo <edson250@hotmail.com>
2018-06-18 11:24:05 -0300

    Login Page

---
+++ lands/Lands/Lands/Infrastructure/InstaceLocator.cs
@@ -0,0 +1,21 @@
+﻿using Lands.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastructure
+{
+    public class InstaceLocator
+    {
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+
+        public InstaceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
