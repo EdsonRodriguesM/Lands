88dee572b61f1c79a68b275a7a7a27b71977edc7
Edson Rodrigues Macedo <edson250@hotmail.com>
2018-06-19 11:15:15 -0300

    update in login

--- lands/Lands/Lands/App.xaml.cs
+++ lands/Lands/Lands/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Lands
 		{
 			InitializeComponent();
 
-			MainPage = new NavigationPage (new LoginPage());
+			this.MainPage = new NavigationPage (new LoginPage());
 		}
 
 		protected override void OnStart ()
