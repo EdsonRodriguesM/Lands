3f871cc78db33881aedea80a5a953f6130b9c859
Edson Rodrigues Macedo <edson250@hotmail.com>
2018-06-21 08:29:44 -0300

    Models

--- lands/Lands/Lands/Models/Language.cs
+++ lands/Lands/Lands/Models/Language.cs
@@ -4,10 +4,13 @@ namespace Lands.Models
 {
     class Language
     {
-        [JsonProperty(PropertyName = "code")]
-        public string iso639_1 { get; set; }
-        public string iso639_2 { get; set; }
-        public string name { get; set; }
-        public string nativeName { get; set; }
+        [JsonProperty(PropertyName = "iso639_1")]
+        public string Iso639_1 { get; set; }
+        [JsonProperty(PropertyName = "iso639_2")]
+        public string Iso639_2 { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "nativeName")]
+        public string NativeName { get; set; }
     }
 }
